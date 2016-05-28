d8e7fba5760e9c0f4c01dad84ba8eb643a32c4a3
jaiz25 <jaizelle@gmail.com>
2016-05-28 08:43:17 +0000

    Medbillpro

--- medbill.py
+++ medbill.py
@@ -8,7 +8,9 @@ import diagnosis
 
 
 NOW = datetime.now()
+#Input patient's name.
 NAME = raw_input('Patient name: ')
+#Input patient's date of birth.
 DATE = raw_input('Date of birth(MM/DD/YYYY): ')
 PROCEDURE_LIST = ['76512', '76514', '76519', '92025', '92235']
 INSURANCE_LIST = ['1199', 'Aetna', 'BCBS', 'Cigna', 'GHI', 'HIP', 'Fidelis',
@@ -16,6 +18,8 @@ INSURANCE_LIST = ['1199', 'Aetna', 'BCBS', 'Cigna', 'GHI', 'HIP', 'Fidelis',
                   'Healthfirst', 'HealthPlus', 'United Healthcare']
 
 
+#Input patient's insurance.  If not on the insurance list, will print out all
+#available insurances.
 while True:
     INSURANCE = raw_input('Insurance: ')
     if INSURANCE in INSURANCE_LIST:
@@ -25,6 +29,8 @@ while True:
         for insurance in INSURANCE_LIST:
             print insurance
 
+#Input procedure code that was performed on the patient.  Shows which procedure
+#codes are availabe.
 while True:
     PROCEDURE = raw_input('Procedure code(76512, 76514, 76519, 92025, 92235):')
     if PROCEDURE not in PROCEDURE_LIST:
@@ -32,6 +38,9 @@ while True:
     else:
         break
 
+#Input diagnosis code. If diagnosis code does not match the procedure code
+#that was entered previously, will list all the diagnosis that are billable
+#for the procedure done.
 while True:
     DIAGNOSIS = raw_input('Diagnosis code: ')
     INVALID_CODE = 'Diagnosis doesn\'t match. Choose from these diagnoses:'
@@ -73,6 +82,7 @@ while True:
                 print keys, values
 
 
+#Prints all the user input on the screen.
 SUPERBILL = '*' * 80 + '\n'
 SUPERBILL += 'SUPERBILL {0:>30} for ' + '{0}/{1}/{2}'.format(NOW.month, NOW.day,
                                                              NOW.year) + '\n'
@@ -85,6 +95,7 @@ SUPERBILL = SUPERBILL.format(NAME, INSURANCE, DATE, PROCEDURE, DIAGNOSIS)
 
 print SUPERBILL
 
+#Saves each superbill to a file named superbill.txt to be printed if needed.
 SAVEFILE = open('superbill.txt', 'w')
 SAVEFILE.write(SUPERBILL)
 SAVEFILE.close()
